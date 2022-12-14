abc1ef74c780479aa65157897448f34291e9b796
iskenderColak <iclk84@gmail.com>
2022-12-14 09:43:03 +0300

    MVC Ticketing Project

---
+++ src/main/java/com/icolak/TicketingProjectMvcApp.java
@@ -0,0 +1,13 @@
+package com.icolak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMvcApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMvcApp.class, args);
+    }
+
+}
