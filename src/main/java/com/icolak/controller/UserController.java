b71efb038b69797d6e6ce531a93d7686f75a0828
iskenderColak <iclk84@gmail.com>
2022-12-14 20:42:22 +0300

    Converter and User save

--- src/main/java/com/icolak/controller/UserController.java
+++ src/main/java/com/icolak/controller/UserController.java
@@ -2,9 +2,12 @@ package com.icolak.controller;
 
 import com.icolak.dto.UserDTO;
 import com.icolak.service.RoleService;
+import com.icolak.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,17 +15,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final RoleService roleService;
+    private final UserService userService;
 
-    public UserController(RoleService roleService) {
+    public UserController(RoleService roleService, UserService userService) {
         this.roleService = roleService;
+        this.userService = userService;
     }
 
     @GetMapping("/create")
     public String createUser(Model model) {
 
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("users", userService.findAll());
         model.addAttribute("roles", roleService.findAll());
 
         return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute UserDTO user) {
+
+        userService.save(user);
+
+        return "redirect:/user/create";
     }
 }
---
+++ src/main/java/com/icolak/converter/RoleDtoConverter.java
@@ -0,0 +1,26 @@
+package com.icolak.converter;
+
+import com.icolak.dto.RoleDTO;
+import com.icolak.service.RoleService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding // not mandatory
+public class RoleDtoConverter implements Converter<String, RoleDTO> {
+// When we are trying to save a user we need RoleDTO for Role column in User List,
+// since th:value="${role.id}" ("1", "2" or "3") in the form is string it gives error.
+// So here we are converting that string to RoleDTO
+    private final RoleService roleService;
+
+    public RoleDtoConverter(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Override
+    public RoleDTO convert(String source) {
+        // Since we need Long for findById method we convert source from string to Long
+        return roleService.findById(Long.parseLong(source));
+    }
+}
