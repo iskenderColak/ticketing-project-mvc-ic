e2c9fc307c6807909656eb3d40ccff559979b4de
iskenderColak <iclk84@gmail.com>
2022-12-14 12:14:25 +0300

    Generic Crud Service

---
+++ src/main/java/com/icolak/service/CrudService.java
@@ -0,0 +1,15 @@
+package com.icolak.service;
+
+import java.util.List;
+
+public interface CrudService<T, ID> {
+
+    T save(T user);
+
+    T findById(ID username);
+
+    List<T> findAll();
+
+    void deleteById(ID username);
+
+}
