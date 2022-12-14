45452b3ac3537b7a5196795d578e2e69be600c13
iskenderColak <iclk84@gmail.com>
2022-12-14 12:18:51 +0300

    Generic Crud Service

--- src/main/java/com/icolak/service/CrudService.java
+++ src/main/java/com/icolak/service/CrudService.java
@@ -5,11 +5,8 @@ import java.util.List;
 public interface CrudService<T, ID> {
 
     T save(T user);
-
     T findById(ID username);
-
     List<T> findAll();
-
     void deleteById(ID username);
 
 }
--- src/main/java/com/icolak/service/RoleService.java
+++ src/main/java/com/icolak/service/RoleService.java
@@ -2,13 +2,6 @@ package com.icolak.service;
 
 import com.icolak.dto.RoleDTO;
 
-import java.util.List;
-
-public interface RoleService {
-
-    RoleDTO save(RoleDTO roleDTO);
-    RoleDTO findById(Long id);
-    List<RoleDTO> findAll();
-    void deleteById(Long id);
+public interface RoleService extends CrudService<RoleDTO, Long>{
 
 }
